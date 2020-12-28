096d51beeb4fd7c31bddb13d60ab3a5e4eb8beb3
viktor <radchenkovictor@yahoo.com>
2020-12-28 20:35:37 +0400

    form styling

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -5,8 +5,14 @@ const menu = document.querySelector('.menu');
 const overlay = document.querySelector('.overlay');
 
 
-menuBtn.addEventListener('click', function() {
+menuBtn && menuBtn.addEventListener('click', function() {
   menuBtn.classList.toggle('menu-btn--active');
   menu.classList.toggle('menu--active');
   overlay.classList.toggle('overlay--active');
 });
+
+overlay && overlay.addEventListener('click', function() {
+  menuBtn.classList.toggle('menu-btn--active');
+  menu.classList.toggle('menu--active');
+  overlay.classList.toggle('overlay--active');
+})
--- app/views/main.py
+++ app/views/main.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, flash
 from flask_login import login_required
 
 main_blueprint = Blueprint('main', __name__)
@@ -7,4 +7,5 @@ main_blueprint = Blueprint('main', __name__)
 @main_blueprint.route('/')
 @login_required
 def index():
+    flash('Welcome to Machine', 'success')
     return render_template('index.html')
