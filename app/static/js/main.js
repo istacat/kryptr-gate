9bb436874743a31f38ed7c542ded1f57f54637b4
viktor <radchenkovictor@yahoo.com>
2020-12-28 19:42:43 +0400

    finalised general ui

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,8 +1,12 @@
 'use strict'
 
 const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const overlay = document.querySelector('.overlay');
 
 
 menuBtn.addEventListener('click', function() {
   menuBtn.classList.toggle('menu-btn--active');
+  menu.classList.toggle('menu--active');
+  overlay.classList.toggle('overlay--active');
 });
