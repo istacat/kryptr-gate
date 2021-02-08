99f25ee1c087d470e802d5e01fd8ed0d0e18ed80
Nikolay <nchernov@gmail.com>
2021-02-07 20:49:45 -0800

    Remote Shell not configured

--- tests/test_remote_powershell.py
+++ tests/test_remote_powershell.py
@@ -1,6 +1,9 @@
+import pytest
 from app.controllers import RemoteShell
+from config import BaseConfig as conf
 
 
+@pytest.mark.skipif(not conf.REMOTE_SHELL_SERVER, reason="Remote Shell not configured")
 def test_simple_ps_commands():
     sh = RemoteShell()
     res = sh.send_command("1+1")
