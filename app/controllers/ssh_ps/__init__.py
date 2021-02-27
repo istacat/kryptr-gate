205442b81bc9f47c70f030ca0f23cf2b6628ea97
Nikolay <nchernov@gmail.com>
2021-02-27 12:50:23 +0100

    remove commented code

--- app/controllers/ssh_ps/__init__.py
+++ app/controllers/ssh_ps/__init__.py
@@ -43,26 +43,17 @@ class RemoteShell:
         BUFFER_SIZE = 1024
         with self.client.get_transport().open_session() as channel:
             channel.get_pty()
-            # channel.invoke_shell()
             channel.settimeout(5)
             channel.exec_command(cmd + "\r\n")
-            # if not channel.send_ready():
-            #     log(log.ERROR, "RemoteShell: Error: cannel does not ready to send")
-            #     raise RemoteShell.Error("cannel does not ready to send")
-            # channel.send(cmd)
-            # channel.send("\n\r")
-            # time.sleep(1)
             if channel.recv_stderr_ready():
                 log(log.ERROR, "RemoteShell: Error")
                 data = channel.recv_stderr(BUFFER_SIZE)
                 error_message = data.decode()
                 raise RemoteShell.Error(error_message)
-            # if channel.recv_ready():
             out_message = ""
             while True:
                 data = channel.recv(BUFFER_SIZE)
                 if not data:
                     break
                 out_message += data.decode()
-            # log(log.DEBUG, "RemoteShell: Output: %s", out_message)
             return out_message
