efb86807921f2a831d865f40ae5eab043153b2a3
tsyboom <tsyboom@gmail.com>
2021-04-13 11:02:41 +0200

    change qrcode data

--- app/controllers/account.py
+++ app/controllers/account.py
@@ -56,6 +56,6 @@ def get_accounts(user):
 
 def create_qrcode(acc):
     """Encode ecc_id and password for chat"""
-    encoded_jwt = jwt.encode({acc.ecc_id: acc.ad_password}, conf.SECRET_KEY, algorithm="HS256")
+    encoded_jwt = jwt.encode({'login': acc.ecc_id, 'password': acc.ad_password}, conf.SECRET_KEY, algorithm="HS256")
     img = qrcode.make(encoded_jwt)
     return img
