1fac1183da9a3adbdf75fc32c1a98e3381099978
Nikolay <nchernov@gmail.com>
2021-04-21 07:34:06 +0300

    redirect to accounts

--- app/views/main.py
+++ app/views/main.py
@@ -1,4 +1,5 @@
-from flask import render_template, Blueprint
+from flask import Blueprint, redirect
+from flask.helpers import url_for
 from flask_login import login_required
 
 main_blueprint = Blueprint('main', __name__)
@@ -7,4 +8,5 @@ main_blueprint = Blueprint('main', __name__)
 @main_blueprint.route('/')
 @login_required
 def index():
-    return render_template('index.html')
+    # return render_template('index.html')
+    return redirect(url_for("account.index"))
